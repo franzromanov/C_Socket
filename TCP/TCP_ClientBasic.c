965461aec35c842c3a56a89e9b8fc8d280a84a61
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-19 21:15:22 +0800

    Update TCP_ClientBasic.c

--- TCP/TCP_ClientBasic.c
+++ TCP/TCP_ClientBasic.c
@@ -3,10 +3,19 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
 
-int main(int argc, char const* argv[]){
+int main(){
+int port;
+char ip[12];
+//ask
+printf("IP ADDRESS: ");
+scanf("%s",ip);
+printf("PORT: ");
+scanf("%d",&port);
+
 //create_socket
  int net_sock;
  net_sock=socket(AF_INET,SOCK_STREAM,0);
@@ -14,16 +23,16 @@ int main(int argc, char const* argv[]){
 //pass_addr
  struct sockaddr_in net_addr;
  net_addr.sin_family=AF_INET;
- net_addr.sin_port=htons(1234);
- inet_aton("192.168.1.8",&net_addr.sin_addr);
+ net_addr.sin_port=htons(port);
+ inet_aton(ip,&net_addr.sin_addr);
 
 //connect
- if(connect(net_sock,(struct sockaddr*)&net_addr,sizeof(net_addr))<0)printf("\n\e[0;31m[!] \e[0;34mconnection error :: server not exist \e[0;31m[!]\n\n");
+ if(connect(net_sock,(struct sockaddr*)&net_addr,sizeof(net_addr))==-1)printf("\n\e[0;31m[!] \e[0;34mconnection error :: server not exist \e[0;31m[!]\n\n");
  else{
 //response
-  char voice[1024];
+  char voice[13];
   recv(net_sock,voice,sizeof(voice),0);
-  printf("[SERVER]:%1024s\n",voice);
+  printf("[SERVER]:%13s\n",voice);
 }
 //esc
  close(net_sock);
